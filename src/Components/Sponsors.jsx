5ad55b8d186d08e9bfdbb5b5c7edb43abf716880
Andrew Xing <xingandrew1@gmail.com>
2017-10-28 00:02:39 -0400

    Updated Carosel Issue

--- src/Components/Sponsors.jsx
+++ src/Components/Sponsors.jsx
@@ -6,7 +6,7 @@ import Header from './Header.jsx'
 import '../Css/sponsors.css';
 import CarouselImage from './CarouselImage.jsx';
 import SponsorBody1 from './SponsorBody1.jsx';
-
+import WATOimage from '../Images/WatoGroupWithCar.jpg';
 
 
 class Sponsors extends Component {
@@ -20,7 +20,9 @@ class Sponsors extends Component {
         <Header
           home = {0}
          />
-        <CarouselImage/>
+            <CarouselImage 
+            image = {WATOimage}
+        />
         <div class="spacer"></div>
         <h1 class="title">Sponsors</h1>
         <p id="blurb">Blurb about sponsors and why they are so important</p>
